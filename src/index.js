154a8b637de12d8966ecf81df54662e56c45ebbc
vivianajr <vivianajr16@gmail.com>
2019-10-25 10:17:31 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const app = express();
+const morgan = require('morgan');
+
+//settings
+app.set('port',process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index'));
+app.use('/api/rutas', require('./routes/rutas'));
+
+
+
+//starting the server
+app.listen (app.get('port'), () => {
+    console.log('Server on port ' + app.get('port'));
+});
+
+
+
+
